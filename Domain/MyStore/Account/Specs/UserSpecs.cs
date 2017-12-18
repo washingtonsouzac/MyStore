3b93ab291feb37f654065dc105fb3c541e072a2f
WashingtonSouza <thewashington.souza@gmail.com>
2017-12-18 01:23:58 -0200

    Implementação de UserSpecs e Expression.

---
+++ Domain/MyStore/Account/Specs/UserSpecs.cs
@@ -0,0 +1,23 @@
+﻿using MyStore.Domain.Account.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Account.Specs
+{
+    public static class UserSpecs
+    {
+        public static Expression<Func<User, bool>> GetByUserName(string username)
+        {
+            return x => x.Username == username;
+        }
+
+        public static Expression<Func<User, bool>> GetByAuthorizationCode(string authorizationCode)
+        {
+            return x => x.AuthorizationCode == authorizationCode;
+        }
+    }
+}
